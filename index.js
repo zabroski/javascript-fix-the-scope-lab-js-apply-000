62cbe5c55165e06c7a20e338168660677c2728a0
Issouf zabre <sheridanzabre@gmail.com>
2018-06-19 04:21:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function myAnimal() {
 }
 
 function yourAnimal() {
+  
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
